645167bd07951af1a5c3685ed41ed22cf2a363d8
chanelkim <kimc889@newschool.edu>
2023-09-07 14:02:03 -0400

    changes t commit

--- coursework/lab01_intro_to_coding/exercises/ex3_dom_manipulation.js
+++ coursework/lab01_intro_to_coding/exercises/ex3_dom_manipulation.js
@@ -29,10 +29,13 @@ console.log(
 
 console.log(viz, viz.children);
 
-const addChildToViz = () => {
+// const addChildToViz = () => {
+  const addChildToViz = (data) => { //IN-CLASS: this is how to connect dataset to this function
   const newChild = document.createElement("div");
   newChild.className = "rectangle";
-  newChild.style.height = Math.random() * 100 + "px";
+  // newChild.style.height = Math.random() * 100 + "px";
+  // newChild.style.height = data.petallength * 20 + "px"; //IN-CLASS: changing math.random to data, adjusting scale; doesn't have access to data, so add as parameter
+    newChild.style.height = data * 20 + "px"; //IN-CLASS: removing ".petallength" makes this more general
   viz.appendChild(newChild);
 };
 
@@ -45,48 +48,66 @@ console.log(`no modifications were necessary in the index.html file`);
 // Where can you see the results of the console.log below? How is it different from in previous exercises?
 console.log(`The data is seen as an array of 150 objects`);
 
-function drawIrisData() {
-  window
-    .fetch("./iris_json.json")
-    .then((data) => data.json())
-    .then((data) => {
-      // Get the 'irisData' div element
-      const irisDataDiv = document.getElementById("irisData");
+// function drawIrisData() {
+//   window
+//     .fetch("./iris_json.json")
+//     .then((data) => data.json())
+//     .then((data) => {
+//       // ----------- CREATES TABLE ---------
+//       // Get the 'irisData' div element
+//       const irisDataDiv = document.getElementById("irisData");
 
-      // Create a table to display the Iris data
-      const table = document.createElement("table");
+//       // Create a table to display the Iris data
+//       const table = document.createElement("table");
 
-      // Create table header
-      const headerRow = table.insertRow(0);
-      for (const key in data[0]) {
-        if (data[0].hasOwnProperty(key)) {
-          const th = document.createElement("th");
-          th.textContent = key;
-          headerRow.appendChild(th);
-        }
-      }
+//       // Create table header
+//       const headerRow = table.insertRow(0);
+//       for (const key in data[0]) {
+//         if (data[0].hasOwnProperty(key)) {
+//           const th = document.createElement("th");
+//           th.textContent = key;
+//           headerRow.appendChild(th);
+//         }
+//       }
 
-      // Create table rows and cells for the data
-      data.forEach((iris, index) => {
-        const row = table.insertRow(index + 1);
-        for (const key in iris) {
-          if (iris.hasOwnProperty(key)) {
-            const cell = row.insertCell();
-            cell.textContent = iris[key];
-          }
-        }
-      });
+//       // Create table rows and cells for the data
+//       data.forEach((iris, index) => {
+//         const row = table.insertRow(index + 1);
+//         for (const key in iris) {
+//           if (iris.hasOwnProperty(key)) {
+//             const cell = row.insertCell();
+//             cell.textContent = iris[key];
+//           }
+//         }
+//       });
 
-      // Append the table to the 'irisData' div
-      irisDataDiv.appendChild(table);
+//       // Append the table to the 'irisData' div
+//       irisDataDiv.appendChild(table);
 
-      console.log(data);
-    });
-}
+//       console.log(data);
+//     });
+// }
 
-drawIrisData();
+// drawIrisData();
 
 // Task
 // Modify the code above to visualize the Iris dataset in the preview of index.html.
 // Feel free to add additional CSS properties in index.html, or using JavaScript, as you see fit.
-/* created a table of data values using chat GPT (accessed Sep 4, 2023)*/
+/*created a table of data values using chat GPT (accessed Sep 4, 2023)*/
+
+/*IN-CLASS, drawing using the iris dataset*/
+function drawIrisData() {
+  window
+    .fetch("./iris_json.json")
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+      data.forEach(callThatAnything => {
+        // addChildToViz(callThatAnything);
+        addChildToViz(callThatAnything.petallength);
+      }
+      )
+    });
+}
+
+drawIrisData();
