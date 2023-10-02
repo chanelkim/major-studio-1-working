48f86f05b11eb42a47a58b27e4f7665501f486a4
chanelkim <kimc889@newschool.edu>
2023-10-02 16:54:29 -0400

    data structure exploration and adding tooltip functionality

--- quant-test/test.js
+++ quant-test/test.js
@@ -8,8 +8,6 @@ var svg = d3.select(".dataviz")
 .append("svg")
     .attr("viewBox", "0 0 " + width + " " + height)
     .attr("preserveAspectRatio", "xMidYMid meet")
-    // .attr("width", width)
-    // .attr("height", height)
 .append("g")
 
 // read json data
@@ -23,7 +21,6 @@ d3.json('data/test.json').then(function(data) {
 
   // Then d3.treemap computes the position of each element of the hierarchy
   d3.treemap()
-    // .tile(tile)
     .size([width, height])
     .padding(2)
     (root)
@@ -39,7 +36,7 @@ d3.json('data/test.json').then(function(data) {
       .attr('width', function (d) { return d.x1 - d.x0; })
       .attr('height', function (d) { return d.y1 - d.y0; })
     //   .style("stroke", "black")
-      .style("fill", "slateblue")
+      // .style("fill", "slateblue")
 
   // and to add the text labels (TITLES)
   const label = svg
@@ -58,4 +55,54 @@ d3.json('data/test.json').then(function(data) {
       // .attr("font-size", ((width+height)/2 * 0.001) + "em")
       .attr("fill", "white")
 
+  // Append a tooltip.
+  const format = d3.format(",d");
+  leaf.append("title")
+      .text(d => `${d.ancestors().reverse().map(d => d.data.name).join(".")}\n${format(d.value)}`);
+
+    //FROM Observable's stdlib - "DOM.uid" (below)
+    var count = 0;
+    function uid(name) {
+      return new Id("O-" + (name == null ? "" : name + "-") + ++count);
+    }
+    function Id(id) {
+      this.id = id;
+      this.href = new URL(`#${id}`, location) + "";
+    }
+    Id.prototype.toString = function() {
+      return "url(" + this.href + ")";
+    };
+
+    //for COLOR
+    const color = d3.scaleOrdinal(data.children.map(d => d.name), 
+    d3.schemeTableau10);
+    // d3.schemeGnBu[9]);
+    // d3.schemeRdBu[8]);
+
+  // Append a color rectangle. 
+  leaf.append("rect")
+      .attr("id", d => (d.leafUid = uid("leaf")).id)
+      .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+      .attr("fill-opacity", 0.6)
+      .attr("width", d => d.x1 - d.x0)
+      .attr("height", d => d.y1 - d.y0);
+
+  // Append a clipPath to ensure text does not overflow.
+  leaf.append("clipPath")
+      .attr("id", d => (d.clipUid = uid("clip")).id)
+    .append("use")
+      .attr("xlink:href", d => d.leafUid.href);
+
+  // Append multiline text. The last line shows the value and has a specific formatting.
+  leaf.append("text")
+      .attr("clip-path", d => d.clipUid)
+    .selectAll("tspan")
+    .data(d => d.data.name.split(/(?=[A-Z][a-z])|\s+/g).concat(format(d.value)))
+    .join("tspan")
+      .attr("x", 3)
+      .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`)
+      .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
+      .text(d => d);
+
+  return Object.assign(svg.node(), {scales: {color}});
 });
