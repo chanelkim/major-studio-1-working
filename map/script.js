334fb1849f2ecaf5fd511929ba8a69188d74c03f
chanelkim <kimc889@newschool.edu>
2023-11-30 17:37:51 -0500

    center and scale map

--- map/script.js
+++ map/script.js
@@ -1,44 +1,56 @@
 // REFERENCES
-// D3 Responsive Zoomable Treemap (D3 v4+) Aleksander Lenart; source: https://codepen.io/figle/pen/qapRZQ
+// Zoom to bounding box, D3 Gallery Observable; source: https://observablehq.com/@d3/zoom-to-bounding-box
 
 // --------------------------------------------------------
 // VARIABLES
 // --------------------------------------------------------
-const width = 1920;
-const height = 1080;
-const size = Math.min(width, height);
-// const aspectRatio = "16:9";
-// const viewBox = "0 0 " + aspectRatio.split(":").join(" ");
+const width = 975;
+const height = 610;
+
+const geoObjectNone = {
+  state: [
+    "Alaska",
+    "Arkansas",
+    "Hawaii",
+    "Idaho",
+    "Indiana",
+    "Mississippi",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "North Carolina",
+    "North Dakota",
+    "Oklahoma",
+    "Oregon",
+    "South Dakota",
+    "West Virginia",
+    "Wyoming",
+  ],
+};
 
 // Draw the map
 function drawMap(us) {
   const land = topojson.feature(us, us.objects.states);
   const interiors = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
+  // const projection = d3.geoIdentity().fitSize([width, height], land);
 
   const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
 
   const svg = d3
     .select("#map")
     .append("svg")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr("viewBox", "0 0 " + size + " " + size)
-    // .attr("viewBox", "0 0 " + aspectRatio.split(":").join(" "))
-    .attr("preserveAspectRatio", "xMidYMid meet")
+    .attr("viewBox", [0, 0, width, height])
+    .attr("width", width)
+    .attr("height", height)
+    .attr("style", "max-width: 100%; height: auto;")
     .on("click", reset);
 
-  const path = d3.geoPath();
+  const path = d3.geoPath().projection(scale(0.9, width, height));
 
   const g = svg.append("g"); // group together with "g"
 
-  //   let width = d3.select("svg").style("width");
-  //   let height = d3.select("svg").style("height");
-  //   width = width.substring(0, width.length - 2);
-  //   height = height.substring(0, height.length - 2);
-
   const states = g
     .append("g")
-    // .attr("transform", "translate(0,0)scale(1)")
     .attr("fill", "#444")
     .attr("cursor", "pointer")
     .selectAll("path")
@@ -57,19 +69,21 @@ function drawMap(us) {
 
   svg.call(zoom);
 
-  //   // Calculate the bounds of the entire SVG content
-  //   const bounds = g.node().getBBox();
-
-  //   // Calculate the translation to center the content
-  //   const translateX = (width - bounds.width) / 2 - bounds.x;
-  //   const translateY = (height - bounds.height) / 2 - bounds.y;
-
-  //   // Apply the calculated translation
-  //   g.attr("transform", "translate(" + translateX + "," + translateY + ")");
-
   // --------------------------------------------------------
   // FUNCTIONS
   // --------------------------------------------------------
+
+  // Scale and center map: https://stackoverflow.com/questions/42430361/scaling-d3-v4-map-to-fit-svg-or-at-all
+  function scale(scaleFactor, width, height) {
+    return d3.geoTransform({
+      point: function (x, y) {
+        this.stream.point(
+          (x - width / 2) * scaleFactor + width / 2,
+          (y - height / 2) * scaleFactor + height / 2
+        );
+      },
+    });
+  }
 
   function reset() {
     states.transition().style("fill", null);
@@ -107,7 +121,7 @@ function drawMap(us) {
           d3.zoomIdentity
             .translate(width / 2, height / 2)
             .scale(
-              Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height))
+              Math.min(8, 0.5 / Math.max((x1 - x0) / width, (y1 - y0) / height))
             )
             .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
           d3.pointer(event, svg.node())
