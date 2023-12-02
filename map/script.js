a6c3c2a239ad91837bc34102f00e7f20d05dfa51
chanelkim <kimc889@newschool.edu>
2023-12-01 23:25:39 -0500

    merged us map json with counts

--- map/script.js
+++ map/script.js
@@ -1,11 +1,19 @@
 // REFERENCES
 // Zoom to bounding box, D3 Gallery Observable; source: https://observablehq.com/@d3/zoom-to-bounding-box
+// Scale and center map; source: https://stackoverflow.com/questions/42430361/scaling-d3-v4-map-to-fit-svg-or-at-all
+// Advanced Mapmaking: Using d3, d3-scale and d3-zoom With Changing Data to Create Sophisticated Maps; source: https://soshace.com/advanced-mapmaking-using-d3-d3-scale-and-d3-zoom-with-changing-data-to-create-sophisticated-maps/
 
 // --------------------------------------------------------
 // VARIABLES
 // --------------------------------------------------------
+// let usdata = d3.json("data/us.json");
+// let statecount = d3.json("data/stateCount.json");
+// console.log(usdata);
+// console.log(statecount);
 const width = 975;
 const height = 610;
+let max;
+let min;
 
 const geoObjectNone = {
   state: [
@@ -29,12 +37,32 @@ const geoObjectNone = {
 };
 
 // Draw the map
-function drawMap(us) {
+function drawMap(us, statecount) {
   const land = topojson.feature(us, us.objects.states);
+  // console.log("Topojson US:", land);
+
+  // Merge the US state data and the state count data
+  const mergedFeatures = _.map(land.features, (feature) => {
+    const stateName = feature.properties.name;
+    const matchingState = _.find(statecount, { geostate: stateName });
+    // If matching state is found, merge the properties; otherwise, keep the original properties
+    const mergedProperties = matchingState
+      ? { ...feature.properties, ...matchingState }
+      : feature.properties;
+    return { ...feature, properties: mergedProperties };
+  });
+  const mergedGeoJSON = {
+    type: "FeatureCollection",
+    features: mergedFeatures,
+  };
+  console.log("Merged US:", mergedGeoJSON);
+  merged = mergedGeoJSON;
+
   const interiors = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
-  // const projection = d3.geoIdentity().fitSize([width, height], land);
 
   const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
+
+  const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([min, max]);
 
   const svg = d3
     .select("#map")
@@ -54,12 +82,26 @@ function drawMap(us) {
     .attr("fill", "#444")
     .attr("cursor", "pointer")
     .selectAll("path")
-    .data(land.features)
+    .data(merged.features)
     .join("path")
     .on("click", clicked)
-    .attr("d", path);
+    .attr("d", path)
+    .attr("class", "state");
 
   states.append("title").text((d) => d.properties.name);
+
+  // states
+  //   .style("fill", (d) => colorScale(d.properties.contrcount))
+  //   .on("mouseover", function (d) {
+  //     d3.select(this).style(
+  //       "fill",
+  //       tinycolor(colorScale(d.properties.contrcount)).darken(10).toString()
+  //     );
+  //   })
+  //   .on("mouseout", function (d) {
+  //     d3.select(this).style("fill", colorScale(d.properties.contrcount));
+  //   });
+  // states.append("title").text((d) => d.properties.contrcount);
 
   g.append("path")
     .attr("fill", "none")
@@ -73,7 +115,7 @@ function drawMap(us) {
   // FUNCTIONS
   // --------------------------------------------------------
 
-  // Scale and center map: https://stackoverflow.com/questions/42430361/scaling-d3-v4-map-to-fit-svg-or-at-all
+  // Scale and center map
   function scale(scaleFactor, width, height) {
     return d3.geoTransform({
       point: function (x, y) {
@@ -143,8 +185,22 @@ function drawMap(us) {
 // --------------------------------------------------------
 async function loadData() {
   const us = await d3.json("data/us.json");
-  console.log(us);
-  drawMap(us);
+  const statecount = await d3.json("data/stateCount.json");
+  const test = await d3.json("data/test.json");
+
+  console.log("US geography data:", us);
+  console.log("State count data:", statecount);
+  drawMap(us, statecount);
+
+  // RENAMING PROPERTY NAMES FOR OBJECT
+  // console.log(test);
+  // data = test;
+  // let newArr = _.map(data, (era) => ({
+  //   catalog_id: +era["catalogid"],
+  //   themes: era["theme"],
+  // }));
+  // console.log(newArr);
 }
 
 loadData();
+// drawMap(usdata, statecountdata);
